7b662c8091a1b7118a2fcb7a60502369a234ccae
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2026-04-19 23:37:03 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,39 @@
-import Library;
+from bresenham import ( bresenham as Bresenham );
+from scipy.spatial.transform import ( Rotation );
 
-LibraryObject = Library.Renderer( Name = "Window", Width = 144, Height = 64 );
-Buffer = LibraryObject.Buffer( );
+from numpy import ( array as Array );
+from time import ( time as Time );
+
+from Library import ( Renderer );
+
+Vertices = Array( [
+    ( -1, -1, -1 ), ( 1, -1, -1 ), ( 1, 1, -1 ), ( -1, 1, -1 ),
+    ( -1, -1, 1 ), ( 1, -1, 1 ), ( 1, 1, 1 ), ( -1, 1, 1 ),
+], dtype = "f4" );
+
+Edges = [
+    ( 0, 1 ), ( 1, 2 ), ( 2, 3 ), ( 3, 0 ),
+    ( 4, 5 ), ( 5, 6 ), ( 6, 7 ), ( 7, 4 ),
+    ( 0, 4 ), ( 1, 5 ), ( 2, 6 ), ( 3, 7 ),
+];
+
+Width, Height = 1344, 720;
+
+LibraryObject = Renderer( Height = Height, Width = Width );
+Angle, Epsilon = 0, ( 1 * .01 );
+
+CenterY = Height * .5;
+CenterX = Width * .5
 
 while ( LibraryObject.Stepper( ) ):
     LibraryObject.Clear( );
+    Angle += Epsilon;
 
-    Buffer.WriteVector2( 20, 20 );
-    Buffer.WriteColor( 255, 0, 0 );
+    Rotated = Vertices @ Rotation.from_euler( "xy", [ Angle, Angle ] ).as_matrix().T;
+    Projection = ( Rotated[ :, : 2 ] / ( Rotated[ :, 2 : 3 ] + 4 ) * CenterY + [ CenterX, CenterY ] ).astype( int );
 
-    LibraryObject.Push( Buffer );
+    for Origin, Destination in Edges:
+        for X, Y in Bresenham( * Projection[ Origin ], * Projection[ Destination ] ):
+            LibraryObject.WriteQuad( X, Y, 1, 1, 255, 255, 255, 255 );
 
-LibraryObject.Exit( );
+    LibraryObject.Update( );
