31794cba3776f5499deaf24af2701fc177896cf7
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2026-04-19 22:21:56 +0200

    im dumb

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ LibraryObject = Library.Renderer( Name = "Window", Width = 144, Height = 64 );
 Buffer = LibraryObject.Buffer( );
 
 while ( LibraryObject.Stepper( ) ):
+    LibraryObject.Clear( );
+
     Buffer.WriteVector2( 20, 20 );
     Buffer.WriteColor( 255, 0, 0 );
 
